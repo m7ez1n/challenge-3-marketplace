3c51f934371a4320f08db60eccc0fc02dc85c3b1
m7Aei <mateuspm2015@hotmail.com>
2019-07-16 13:47:24 -0300

    Configurando as rotas

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,23 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+
 const controllers = require('./app/controllers')
 
 routes.post('/users', controllers.UserController.store)
 routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware)
+
+/*
+  Ad's
+*/
+
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
